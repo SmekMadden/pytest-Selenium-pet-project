164f42a731a4c4b1ae3c6d8bed3bbda72c10fa58
Vlad Smek <smek1negithub@gmail.com>
2024-03-18 09:32:03 +0300

    Fixed long loading via page loading strategy

--- conftest.py
+++ conftest.py
@@ -2,10 +2,16 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options as ChromeOptions
 
 
-@pytest.fixture(scope="class")
+@pytest.fixture(scope="function")
 def driver():
-    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+    chrome_options = ChromeOptions()
+    chrome_options.page_load_strategy = "eager"
+    driver = webdriver.Chrome(
+        service=ChromeService(ChromeDriverManager().install()),
+        options=chrome_options,
+    )
     yield driver
     driver.quit()
